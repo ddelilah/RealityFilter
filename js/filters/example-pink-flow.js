bc86e8b32db54b07b9c1c227b02e9ae922377c3d
Dan.Manastireanu <Dan.Manastireanu@isdc.eu>
2016-07-07 18:06:34 +0300

    Add pink-flow example that uses a transparent canvas

--- js/filters/example-pink-flow.js
+++ js/filters/example-pink-flow.js
@@ -1,54 +1,66 @@
 (function(){
 
-var overlay = document.createElement('canvas');
-    overlay.width = 2048;
-    overlay.height = 1024;
+var pinkCanvas = document.createElement('canvas');
 
-var overlayContext = overlay.getContext('2d');
+pinkCanvas.width = 1000;
+		pinkCanvas.height = 1000;
+		var pinkContext = pinkCanvas.getContext('2d');
 
-var temp = document.createElement('canvas');
-    temp.width = 2048;
-    temp.height = 1024;
+function isMagenta(r,g,b) {
+    var threshold = 20,
+      dx = r - 255,
+      dy = g - 0,
+      dz = b - 255;
 
-var tempContext = temp.getContext('2d');
+    if ((r - g) >= threshold && (b - g) >= threshold) {
+      return true;
+    }
+    return dx * dx + dy * dy + dz * dz < 19600;
+}
 
-function nextPowerOf2(x) {
-      return Math.pow(2, Math.ceil(Math.log(x) / Math.log(2)));
-  }
+function saturate(d,i,r,g,b) {
+    var value = 1.75;
+    var gray = 0.2989*r + 0.5870*g + 0.1140*b; //weights from CCIR 601 spec
+    d[i] = clamp(-gray * value + r * (1+value));
+    d[i+1] = clamp(-gray * value + g * (1+value));
+    d[i+2] = clamp(-gray * value + b * (1+value));
+}
 
-function fadeOut(ctx) {
-   var imageData = ctx.getImageData(0, 0, overlay.width, overlay.height);
-   var data = imageData.data;
-
-   for (var i = 0; i < data.length; i += 4) {
-        var red = data[i],
-            green = data[i + 1],
-            blue = data[i + 2],
-            alpha = data[i + 3];
-
-        data[i + 3] = Math.round(0.9 * alpha);
-   }
-   ctx.putImageData(imageData, 0, 0);
+function clamp(color) {
+    return Math.min(Math.max(Math.floor(color),0),255)
 }
 
 window.filters.push({
-	name : 'inverse',
-	author : 'patcat',
+	name : 'pink flow',
+	author : 'danmana',
 	draw : function (canvas, context) {
 
 	    // get the raw image data
 		var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 		var data = imageData.data;
 
+
+
+		var pinkImageData = pinkContext.getImageData(0, 0, canvas.width, canvas.height);
+		var pinkData = pinkImageData.data;
+
         // ---------- Apply filter --------- //
 		for (var i = 0; i < data.length; i += 4) {
 			var red = data[i],
                 green = data[i + 1],
                 blue = data[i + 2],
-                alpha = data[i + 3];
+                alpha = pinkData[i + 3];
 
-            if (red < 200) {
-                data[i + 3] = 0;
+            // pink like
+            if (isMagenta(red,green,blue)) {
+                saturate(pinkData, i, red, green, blue);
+//                pinkData[i] = red;
+//                pinkData[i+1] = green;
+//                pinkData[i+2] = blue;
+                pinkData[i+3] = 255;
+            } else {
+                // fade it out
+                 pinkData[i+3] = Math.floor(alpha * 0.95);
             }
 
 
@@ -56,11 +68,9 @@ window.filters.push({
 
 
         // paint the new data
-		imageData.data = data;
-		tempContext.putImageData(imageData, 0, 0);
-		overlayContext.drawImage(temp, 0, 0);
-		fadeOut(overlayContext);
-		context.drawImage(overlay, 0, 0);
+        pinkImageData.data = pinkData;
+		pinkContext.putImageData(pinkImageData, 0, 0);
+		context.drawImage(pinkCanvas, 0, 0);
 
 	}
 });
